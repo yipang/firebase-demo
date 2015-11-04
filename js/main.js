bf4794401b1057474d371957e1eb6e54d95e417c
Yi Pang <pangyibobo@gmail.com>
2015-11-04 10:17:58 -0800

    haha

--- js/main.js
+++ js/main.js
@@ -1,4 +1,87 @@
 // Create application with dependency 'firebase'
+var myApp = angular.module('myApp', ['firebase'])
+myApp.controller('myCtrl', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject){
+    var ref = new Firebase('https://firs.firebaseio.com/');
+    var tweetRef = ref.child("tweets");
+    var userRef = ref.child("users");
+
+    $scope.tweets = $firebaseArray(tweetRef);
+    $scope.users = $firebaseObject(userRef);
+
+    // Create authorization object that referes to firebase
+    $scope.authObj = $firebaseAuth(ref);
+
+    // Test if already logged in
+    var authData = $scope.authObj.$getAuth();
+    if (authData) {
+        $scope.userId = authData.uid;
+    } 
+
+    // SignUp function
+    $scope.signUp = function() {
+        // Create user
+        $scope.authObj.$createUser({
+            email: $scope.email,
+            password: $scope.password,          
+        })
+
+        // Once the user is created, call the logIn function
+        .then($scope.logIn)
+
+        // Once logged in, set and save the user data
+        .then(function(authData) {
+            $scope.userId = authData.uid;
+            $scope.users[authData.uid] ={
+                handle:$scope.handle, 
+                userImage:$scope.userImage,
+            }
+            $scope.users.$save()
+        })
+
+        // Catch any errors
+        .catch(function(error) {
+            console.error("Error: ", error);
+        });
+    }
+
+    // SignIn function
+    $scope.signIn = function() {
+        $scope.logIn().then(function(authData){
+            $scope.userId = authData.uid;
+        })
+    }
+
+    // LogIn function
+    $scope.logIn = function() {
+        return $scope.authObj.$authWithPassword({
+            email: $scope.email,
+            password: $scope.password
+        })
+    }
+
+    // LogOut function
+    $scope.logOut = function() {
+        $scope.authObj.$unauth()
+        $scope.userId = false
+    }
+
+    $scope.tweet = function() {
+    $scope.tweets.$add({
+        text:$scope.newTweet,
+        userId:$scope.userId,
+        likes:0,
+        time:Firebase.ServerValue.TIMESTAMP
+    }).then(function() {
+        $scope.newTweet =''
+        $scope.tweets.$save()
+    })
+    }
+
+    $scope.like = function(tweet) {
+        tweet.likes +=1;
+        $scope.tweets.$save()
+    }
+})
 
 
 // Bind controller, passing in $scope, $firebaseAuth, $firebaseArray, $firebaseObject
